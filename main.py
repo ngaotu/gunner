709d9109921aa1b96f929549a283397f1fe9daca
Tung Nguyen <ngaotu662003@gmail.com>
2022-10-18 15:23:01 +0700

    Commit 3: Update map + diamon coins

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+
 import random
+
 import pygame,sys
 import os
 import csv
@@ -6,13 +8,16 @@ pygame.init()
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 GRAVITY = 0.5
+SCROLL = 200
 # so hang so cot cua map
 ROWS = 16
 COLS = 150
 # kich thuoc cua 1 o
 TILE_SIZE = SCREEN_HEIGHT//ROWS
-TILE_TYPE = 20
+TILE_TYPE = 21
+MAX_LEVEL =2
 levels =1
+total_diamon = 0
 # Font 
 font = pygame.font.SysFont('Futura',25)
 # khoi tao cua so tro choi
@@ -20,6 +25,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
 pygame.display.set_caption(" Super Shooter")
 clock = pygame.time.Clock() 
 run = True
+start_game =False
 move_left = False
 move_right = False
 shoot = False
@@ -48,14 +54,20 @@ items_box = {
     'Ammo'  : ammo_box_img
 }
 # background 
+diamon_display_img = pygame.image.load('D:/Workspace/game/img/diamon.png').convert_alpha()
 heart_img = pygame.image.load('D:/Workspace/game/img/icons/heart.png').convert_alpha()
 pine1_img = pygame.image.load('D:/Workspace/game/img/Background/pine1.png').convert_alpha()
 pine2_img = pygame.image.load('D:/Workspace/game/img/Background/pine2.png').convert_alpha()
 mountain_img = pygame.image.load('D:/Workspace/game/img/Background/mountain.png').convert_alpha()
 sky_img = pygame.image.load('D:/Workspace/game/img/Background/sky_cloud.png').convert_alpha()
+start_img = pygame.image.load("D:/Workspace/game/img//start_btn.png").convert_alpha()
+restart_img = pygame.image.load("D:/Workspace/game/img//restart_btn.png").convert_alpha()
+exit_img = pygame.image.load("D:/Workspace/game/img//exit_btn.png").convert_alpha()
 BG = (144, 201, 120)
 RED = (255,0,0)
 BLACK = (252,252,255)
+
+
 def draw_bg():
 	screen.fill(BG)
 	width = sky_img.get_width()
@@ -63,12 +75,27 @@ def draw_bg():
 		screen.blit(sky_img, ((x * width) - bg_scroll * 0.5, 0))
 		screen.blit(mountain_img, ((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))
 		screen.blit(pine1_img, ((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
-		screen.blit(pine2_img, ((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
-
-def draw_charecter(text,font,text_col,x,y):
-    img = font.render(text,True,text_col)
+		screen.blit(pine2_img, ((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height())) 
+def draw_charecter(text1,text2,font,text_col,x,y):
+    heart = font.render(text1,True,text_col)
+    diamon = font.render(text2,True,text_col)
     screen.blit(heart_img,(0,0))
-    screen.blit(img,(x+heart_img.get_width(),y))
+    screen.blit(heart,(x+heart_img.get_width(),y))
+    screen.blit(diamon_display_img,(0,heart_img.get_height()))
+    screen.blit(diamon,(diamon_display_img.get_width()+x,heart_img.get_height()+y))
+def reset_level():
+    enemy_group.empty()
+    bullet_group.empty()
+    items_box_group.empty()
+    water_group.empty()
+    decoration_group.empty()
+    exit_group.empty()
+    diamon_group.empty()
+    map_data = []
+    for row in range(ROWS):
+        r =[-1]*COLS
+        map_data.append(r)
+    return map_data
 # khoi tao lop chien binh
 class Soldier(pygame.sprite.Sprite):
     def __init__(self, pos_x , pos_y,typeOf ,scale,speed):
@@ -92,6 +119,7 @@ class Soldier(pygame.sprite.Sprite):
         self.type = typeOf
         self.index = 0 
         self.action = 0
+        self.diamon = 0
         #  list chua cac hinh anh cua doi tuong 
         self.animation_list = []
         self.update_time = pygame.time.get_ticks()
@@ -110,12 +138,17 @@ class Soldier(pygame.sprite.Sprite):
         self.image = self.animation_list[self.action][self.index]
         self.rect = self.image.get_rect()
         self.rect.center = (pos_x,pos_y)
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
     def display(self):
         screen.blit(pygame.transform.flip(self.image,self.flip,False),self.rect)
         pygame.draw.rect(screen,(155,155,155),self,1)
     def moving(self,move_left,move_right):
         dx = 0
         dy = 0
+        screen_scroll =0
+        level_complete = False
+        global total_diamon
         #move left
         if move_left:
             dx = -self.speed
@@ -128,21 +161,46 @@ class Soldier(pygame.sprite.Sprite):
             self.direction = 1
         #jump
         if self.jump ==True and self.jump_limit ==False:
-            self.v = -8
+            self.v = -10
             self.jump = False
             self.jump_limit = True
         # them trong luc va gioi han van toc toi da
         self.v += GRAVITY
-        if self.v>7 :
-            self.v =7
+        if self.v>8 :
+            self.v =8
         dy +=self.v   
         # kiem tra va cham 
-        if self.rect.bottom + dy > 400:
-            dy = 400-self.rect.bottom 
-            self.jump_limit = False
-        # cap nhat vi tri moi cua soldier
+        for tile in  new_map.obstacle_lits:
+            #kiem tra va cham theo chieu ngang
+            if tile[1].colliderect(self.rect.x+dx,self.rect.y,self.width,self.height):
+                dx = 0
+                # if tile[0] == img_list[12]:
+                #     tile[1].x -=dx
+            if tile[1].colliderect(self.rect.x,self.rect.y+dy,self.width,self.height):
+                if self.v >= 0:
+                    self.v = 0
+                    self.jump_limit = False
+                    dy = tile[1].top-self.rect.bottom
+                else:
+                    self.v = 0
+                    dy = tile[1].bottom - self.rect.top
+     
+        if self.rect.top > SCREEN_HEIGHT:
+            self.health = 0
+
         self.rect.x += dx
         self.rect.y += dy
+        if self.type =='player':
+            if (self.rect.right > SCREEN_WIDTH-SCROLL and bg_scroll<= COLS*TILE_SIZE-SCREEN_WIDTH) or (self.rect.left <SCROLL and bg_scroll>abs(dx)):
+                self.rect.x -=dx
+                screen_scroll =-dx
+            if self.rect.left+dx<=0 or self.rect.right+dx>=SCREEN_WIDTH:
+                dx = 0
+            if pygame.sprite.spritecollide(self,exit_group,False):
+                # if enemy_group.has()==False:
+                level_complete = True
+                total_diamon += self.diamon
+        return screen_scroll,level_complete
  # cap nhat hoat anh cho nhan vat       
     def update_animation(self):
         COUNTDOWN = 100
@@ -177,7 +235,7 @@ class Soldier(pygame.sprite.Sprite):
     def shoot(self):
         if self.shoot_cooldown ==0:
             self.shoot_cooldown=15
-            bullets = Bullet(self.rect.centerx+(30*self.direction),self.rect.centery,self.direction,10)
+            bullets = Bullet(self.rect.centerx+(29*self.direction),self.rect.centery,self.direction,10)
             bullet_group.add(bullets)
 class Bullet(pygame.sprite.Sprite):
     def __init__(self,x,y,direction,speed):
@@ -201,6 +259,10 @@ class Bullet(pygame.sprite.Sprite):
             if player1.alive:
                 self.kill()
                 player1.health-=5
+        for tile in  new_map.obstacle_lits:
+            #kiem tra va cham theo chieu ngang
+            if tile[1].colliderect(self.rect):
+                self.kill()
 class ItemBox(pygame.sprite.Sprite):
     def __init__(self,x,y,it_type):
         pygame.sprite.Sprite.__init__(self)
@@ -209,6 +271,7 @@ class ItemBox(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.midtop = (x + TILE_SIZE//2,y + (TILE_SIZE- self.image.get_height()))
     def update(self):
+        self.rect.x +=screen_scroll
         if pygame.sprite.collide_rect(self,player1):
             if self.it_type =='Health':
                 player1.health =min(player1.health+50,player1.max_health)
@@ -228,6 +291,14 @@ class Enemy(Soldier):
             bullets = Bullet(self.rect.centerx+(0.75 * self.rect.size[0] * self.direction),self.rect.centery,self.direction,5)
             bullet_group.add(bullets)
         # cai dat phuong thuc di chuyen cua ai
+    def moving(self, move_left, move_right):
+        dx = 0
+        dy = 0
+        for tile in  new_map.obstacle_lits:
+            #kiem tra va cham theo chieu ngang
+            if tile[1].colliderect(self.rect.x+dx,self.rect.y,self.width,self.height):
+                self.direction*=-1
+        return super().moving(move_left, move_right)
     def automatic(self,player1):
         if self.alive and player1.alive:
             if random.randint(1,300) ==1:
@@ -258,24 +329,35 @@ class Enemy(Soldier):
                     self.idle =False
         else:
             self.update_action(3)
+        self.rect.x +=screen_scroll
+         
 class Decoration(pygame.sprite.Sprite):
     def __init__(self,x,y,img):
         pygame.sprite.Sprite.__init__(self)
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.midtop = (x + TILE_SIZE//2,y + (TILE_SIZE- self.image.get_height()))
+    def update(self):
+        self.rect.x +=screen_scroll
 class Water(pygame.sprite.Sprite):
     def __init__(self,x,y,img):
         pygame.sprite.Sprite.__init__(self)
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.midtop = (x + TILE_SIZE//2,y + (TILE_SIZE- self.image.get_height()))
+    def update(self):
+        self.rect.x +=screen_scroll
+        if self.rect.colliderect(player1.rect.x,player1.rect.y -player1.width//2,player1.width,player1.height):
+            player1.health = 0
+            
 class Exit_level(pygame.sprite.Sprite):
     def __init__(self,x,y,img):
         pygame.sprite.Sprite.__init__(self)
         self.image = img
         self.rect = self.image.get_rect()
         self.rect.midtop = (x + TILE_SIZE//2,y + (TILE_SIZE- self.image.get_height()))
+    def update(self):
+        self.rect.x +=screen_scroll
 class Map():
     def __init__(self):
         self.obstacle_lits = []
@@ -289,14 +371,14 @@ class Map():
                     img_rect.y = y*TILE_SIZE
                     tile_data = (img,img_rect)
                     #ground
-                if 0<= tile <=8:
+                if 0<= tile <=8 or tile ==12:
                     self.obstacle_lits.append(tile_data)
                     #water
                 elif 9<=tile<=10:
                     water = Water(x*TILE_SIZE,y*TILE_SIZE,img)
                     water_group.add(water)
                     #decorate
-                elif 11<=tile<=14:
+                elif 11<=tile<=14 and tile != 12:
                     decorate = Decoration(x*TILE_SIZE,y*TILE_SIZE,img)
                     decoration_group.add(decorate)
                 #player
@@ -316,76 +398,150 @@ class Map():
                 elif tile ==19:
                     exit_level = Exit_level(x*TILE_SIZE,y*TILE_SIZE,img)
                     exit_group.add(exit_level)
+                elif tile ==20:
+                    diamon = Diamons(x*TILE_SIZE,y*TILE_SIZE,img)
+                    diamon_group.add(diamon)
         return player1
     def draw(self):
         for tile in self.obstacle_lits:
+            tile[1][0] +=screen_scroll
             screen.blit(tile[0],tile[1])
-        
+class Diamons(pygame.sprite.Sprite):
+    def __init__(self,x,y,img):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.midtop = (x + TILE_SIZE//2,y + (TILE_SIZE- self.image.get_height()))
+    def update(self):
+        self.rect.x +=screen_scroll 
+        if self.rect.colliderect(player1.rect):
+            player1.diamon+=1
+            self.kill()
 
+class Button():
+    def __init__(self,img,x,y,scale):
+        self.image = pygame.transform.scale(img,(img.get_width()*scale,img.get_height()*scale))
+        self.rect = self.image.get_rect()
+        self.rect.topleft =(x,y)
+        self.clicked = False
+    def draw(self,surface):
+        action = False
+        pos = pygame.mouse.get_pos()
+        if self.rect.collidepoint(pos):
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked ==False:
+                self.clicked =True
+                action = True
+            elif pygame.mouse.get_pressed()[0] ==0:
+                self.clicked = False
+        surface.blit(self.image,(self.rect.x,self.rect.y))
+        return action
 bullet_group = pygame.sprite.Group()
 enemy_group = pygame.sprite.Group()
 items_box_group = pygame.sprite.Group()
 decoration_group = pygame.sprite.Group()
 water_group = pygame.sprite.Group()
 exit_group = pygame.sprite.Group()
-item_box = ItemBox(200,300,'Health')
-items_box_group.add(item_box)
+diamon_group = pygame.sprite.Group()
+
+#create button
+start_btn = Button(start_img,SCREEN_WIDTH//2-300,SCREEN_HEIGHT//2-150,1)
+restart_btn = Button(restart_img,start_btn.rect.x+100,100,1)
+exit_btn = Button(exit_img,SCREEN_WIDTH//2-200+start_img.get_width(),SCREEN_HEIGHT//2-150,1)
 
 
-# bg =pygame.image.load('D:/Workspace/game/img/background/mountain.png')
-# rect_bg =  bg.get_rect()
-# rect_bg.center =((0,0))
 
 # TAO BAN DO
-# khoi tao ban do rong
-map_data = []
-for row in range(ROWS):
-    r =[-1]*COLS
-    map_data.append(r)
-# them du lieu vao ban do
+map_data = reset_level()
 with open(f"D:/Workspace/game/level{levels}_data.csv",newline='') as f:
-    reader = csv.reader(f,delimiter=',')
-    for x,row in enumerate(reader):
-        for y,tile in enumerate(row):
-            map_data[x][y] = int(tile)
+                    reader = csv.reader(f,delimiter=',')
+                    for x,row in enumerate(reader):
+                        for y,tile in enumerate(row):
+                            map_data[x][y] = int(tile)
 new_map = Map()
 player1 =new_map.load_data(map_data)
-print(map_data)
+
 while run:
-    draw_bg()
-    new_map.draw()
-    draw_charecter(f': {player1.health}',font,BLACK,4,4)
-    player1.update()
-    player1.display()
-    for enemy in enemy_group:
-        enemy.automatic(player1)
-        enemy.update()
-        if enemy.disappear>0:
-            enemy.display()
-    #hien thi tat ca dan
-    bullet_group.update()
-    items_box_group.update() 
-    decoration_group.update() 
-    water_group.update() 
-    exit_group.update() 
-    bullet_group.draw(screen)
-    items_box_group.draw(screen)
-    decoration_group.draw(screen) 
-    water_group.draw(screen)
-    exit_group.draw(screen)  
-    if player1.alive:
-        if shoot:
-            player1.shoot()
-        if player1.jump_limit:
-            player1.update_action(2)
-        elif move_left or move_right:
-            player1.update_action(1)
-        else:
-            player1.update_action(0)
-        player1.moving(move_left,move_right)
+    if start_game==False:
+        draw_bg()
+        if start_btn.draw(screen):
+            start_game =True
+        if exit_btn.draw(screen):
+            run = False
     else:
-        pass
-    
+        draw_bg()
+        new_map.draw()
+        draw_charecter(f': {player1.health}',f': {player1.diamon}',font,BLACK,4,4)
+        player1.update()
+        player1.display()
+        for enemy in enemy_group:
+            enemy.automatic(player1)
+            enemy.update()
+            if enemy.disappear>0:
+                enemy.display()
+        #hien thi tat ca dan
+        bullet_group.update()
+        items_box_group.update()
+        diamon_group.update()
+        decoration_group.update() 
+        water_group.update() 
+        exit_group.update() 
+        bullet_group.draw(screen)
+        items_box_group.draw(screen)
+        diamon_group.draw(screen)
+        decoration_group.draw(screen) 
+        water_group.draw(screen)
+        exit_group.draw(screen)  
+        if player1.alive:
+            if shoot:
+                player1.shoot()
+            if player1.jump_limit:
+                player1.update_action(2)
+            elif move_left or move_right:
+                player1.update_action(1)
+            else:
+                player1.update_action(0)
+            screen_scroll,level_complete=player1.moving(move_left,move_right)
+            print(total_diamon)
+            bg_scroll -=screen_scroll
+            if level_complete:
+                screen_scroll = 0
+                levels+=1
+                if levels<=MAX_LEVEL:
+                    bg_scroll =0
+                    map_data = reset_level()
+                    with open(f"D:/Workspace/game/level{levels}_data.csv",newline='') as f:
+                        reader = csv.reader(f,delimiter=',')
+                        for x,row in enumerate(reader):
+                            for y,tile in enumerate(row):
+                                map_data[x][y] = int(tile)
+                    new_map = Map()
+                    player1 =new_map.load_data(map_data)
+                if levels > MAX_LEVEL:
+                    start_game =False
+                    levels = 1
+                    bg_scroll =0
+                    map_data = reset_level()
+                    with open(f"D:/Workspace/game/level{levels}_data.csv",newline='') as f:
+                        reader = csv.reader(f,delimiter=',')
+                        for x,row in enumerate(reader):
+                            for y,tile in enumerate(row):
+                                map_data[x][y] = int(tile)
+                    new_map = Map()
+                    player1 =new_map.load_data(map_data)
+        else:
+            screen_scroll =0
+            if restart_btn.draw(screen):
+                bg_scroll =0
+                map_data = reset_level()
+                with open(f"D:/Workspace/game/level{levels}_data.csv",newline='') as f:
+                    reader = csv.reader(f,delimiter=',')
+                    for x,row in enumerate(reader):
+                        for y,tile in enumerate(row):
+                            map_data[x][y] = int(tile)
+                new_map = Map()
+                player1 =new_map.load_data(map_data)
+            if exit_btn.draw(screen):
+                run =False
     for event in pygame.event.get():
         #quit game
         if event.type == pygame.QUIT:
